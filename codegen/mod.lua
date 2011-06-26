e2493592e18dff217df50ed558e63ad0b4e204d2
Kun Wang <ifreedom.cn@gmail.com>
2011-06-26 22:36:17 +0800

    Move codegen bin file to bin dir.
    Move generator.lua, gir-parser.lua and update-version.lua
     to bin directory.
    
    Change mod.lua for build.

--- codegen/mod.lua
+++ codegen/mod.lua
@@ -1,9 +1,12 @@
-local es = expandStr
 shell( es'$INSTD $DEST/$SHARED' )
+
 shell( es'$INSTD $DEST/bin' )
-shell( es'$INST codegen/src/*.lua $DEST/$SHARED' )
-shell( es'$INST codegen/src/generator.lua $DEST/bin/lgob-generator' )
-shell( es'$INST codegen/src/gir-parser.lua $DEST/bin/lgob-gir-parser' )
+LOCAL_BIN = 'codegen/bin'
+shell( es'$INST $LOCAL_BIN/generator.lua $DEST/bin/lgob-generator' )
+shell( es'$INST $LOCAL_BIN/gir-parser.lua $DEST/bin/lgob-gir-parser' )
+shell( es'$INST $LOCAL_BIN/update-version.lua $DEST/bin/lgob-update-def' )
 shell( es'$CHMOD $DEST/bin/lgob-generator' )
 shell( es'$CHMOD $DEST/bin/lgob-gir-parser' )
-shell( es'$CHMOD $DEST/$SHARED/generator.lua' )
+shell( es'$CHMOD $DEST/bin/lgob-update-def' )
+
+shell( es'$INST codegen/src/*.lua $DEST/$SHARED' )
